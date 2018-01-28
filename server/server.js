943c17e1ebe3b26730f899b6e6b31f076d216611
John Hebron <johnhebron@gmail.com>
2018-01-28 06:39:04 -0600

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -15,12 +15,22 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log("new user connected");
 
+  // socket.emit from admin text welcome to the chat App
+  socket.emit("newMessage", {from: "Admin", text: "Welcome to the chat app", createdAt: new Date().getTime()});
+  // socket.broadcast.emit from admin text new user joined
+  socket.broadcast.emit("newMessage", {from: "Admin", text: "New user joined", createdAt: new Date().getTime()});
+
   socket.on('createMessage', (message) => {
     io.emit("newMessage", {
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
     });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', (socket) => {
